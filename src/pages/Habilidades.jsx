3e1d36b45bf20ae41f42cc391dc99a6c8d1791a0
Joaquin Meneses <joakinmeneses.jm@gmail.com>
2023-06-28 18:14:41 -0300

    table skills

--- src/pages/Habilidades.jsx
+++ src/pages/Habilidades.jsx
@@ -2,15 +2,55 @@ import React from "react";
 
 export default function Habilidades() {
   return (
-    <div className="hero min-h-screen bg-[#293038]" id="acerca">
-      <div className="hero-content text-center text-neutral-content">
-        <div className="max-w-md text-white">
-          <p className="mb-5">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
-            rerum, odio explicabo possimus eaque corporis. Veritatis modi iure
-            doloremque, aliquid dignissimos tempora, similique est adipisci
-            error beatae quas natus commodi?{" "}
-          </p>
+    <div className="hero min-h-screen bubbles" id="habilidades">
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content items-start">
+        <div>
+          <p className="text-2xl	font-semibold border px-2 ">Lenguajes</p>
+          <div className="border px-2 cursor-default">
+            <p className="hover:font-bold hover:text-primary">Javascript</p>
+          </div>
+        </div>
+        <div>
+          <p className="text-2xl	font-semibold border px-2 ">Frontend</p>
+          <div className="border px-2 cursor-default">
+            <p className="hover:font-bold hover:text-primary">HTML</p>
+            <p className="hover:font-bold hover:text-primary">CSS</p>
+            <p className="hover:font-bold hover:text-primary">React</p>
+          </div>
+        </div>
+        <div>
+          <p className="text-2xl	font-semibold border px-2 ">Backend</p>
+          <div className="border px-2 cursor-default">
+            <p className="hover:font-bold hover:text-primary">Mongo</p>
+            <p className="hover:font-bold hover:text-primary">Node.js</p>
+          </div>
+        </div>
+        <div>
+          <p className="text-2xl	font-semibold border px-2 ">Herramientas</p>
+          <div className="border px-2 cursor-default">
+            <p className="hover:font-bold hover:text-info">Metodologia AGILE</p>
+            <p className="hover:font-bold hover:text-info">
+              GitHub colaborativo
+            </p>
+            <p className="hover:font-bold hover:text-info">GIT</p>
+            <p className="hover:font-bold hover:text-info">Zustand</p>
+            <p className="hover:font-bold hover:text-info">Redux</p>
+            <p className="hover:font-bold hover:text-info">Bootstrap</p>
+            <p className="hover:font-bold hover:text-info">Tailwind</p>
+            <p className="hover:font-bold hover:text-info">Mongoose</p>
+            <p className="hover:font-bold hover:text-info">Postman</p>
+            <p className="hover:font-bold hover:text-info">Atlassian</p>
+            <p className="hover:font-bold hover:text-info">Trello</p>
+          </div>
+        </div>
+        <div>
+          <p className="text-2xl	font-semibold border px-2 ">En aprendizaje</p>
+          <div className="border px-2 cursor-default">
+            <p className="hover:font-bold hover:text-warning">Astro</p>
+            <p className="hover:font-bold hover:text-warning">SQL</p>
+            <p className="hover:font-bold hover:text-warning">React Query</p>
+          </div>
         </div>
       </div>
     </div>
