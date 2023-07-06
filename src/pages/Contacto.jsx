cd97b98efc73c546853f7ed7f0393aff06010f32
Joaquin Meneses <joakinmeneses.jm@gmail.com>
2023-07-06 17:19:14 -0300

    edit Contact and Skills

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -5,12 +5,12 @@ export default function Contacto() {
     <div className="hero min-h-screen bg-[#293038]" id="acerca">
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md text-white">
-          <p className="mb-5">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi
-            dolor consectetur nam ex eaque explicabo eligendi similique sed.
-            Aut, accusantium. Vel culpa ab officia ex sit dolor laborum rem
-            voluptas?{" "}
-          </p>
+          <p className="mb-5">En desarrollo</p>
+          <progress className="progress w-56" value={0} max="100"></progress>
+          <progress className="progress w-56" value="10" max="100"></progress>
+          <progress className="progress w-56" value="40" max="100"></progress>
+          <progress className="progress w-56" value="70" max="100"></progress>
+          <progress className="progress w-56" value="100" max="100"></progress>
         </div>
       </div>
     </div>
--- src/pages/Experiencia.jsx
+++ src/pages/Experiencia.jsx
@@ -5,12 +5,12 @@ export default function Experiencia() {
     <div className="hero min-h-screen bg-[#293038]" id="acerca">
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md text-white">
-          <p className="mb-5">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
-            rerum, odio explicabo possimus eaque corporis. Veritatis modi iure
-            doloremque, aliquid dignissimos tempora, similique est adipisci
-            error beatae quas natus commodi?{" "}
-          </p>
+          <p className="mb-5">En desarrollo</p>
+          <progress className="progress w-56" value={0} max="100"></progress>
+          <progress className="progress w-56" value="10" max="100"></progress>
+          <progress className="progress w-56" value="40" max="100"></progress>
+          <progress className="progress w-56" value="70" max="100"></progress>
+          <progress className="progress w-56" value="100" max="100"></progress>
         </div>
       </div>
     </div>
