229a8be7f508867572e935f8d32f2d7221feb306
Joaquin Meneses <joakinmeneses.jm@gmail.com>
2023-07-07 20:23:25 -0300

    Fix temporary: card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Card({ text, photo, description, link }) {
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
+    <div className="card w-96 bg-base-100 shadow-xl hover:border">
       <figure className="px-10 pt-10">
         <img src={photo} alt={text} className="rounded-xl" />
       </figure>
