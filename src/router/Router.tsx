cab2398a066ff5315f9f1ae6c318b88c7e214120
Joaquin Meneses <joakinmeneses.jm@gmail.com>
2023-07-19 18:11:22 -0300

    Move page 404 a folder Public

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import App from "../App";
-import NotFoundPage from "../pages/NotFoundPage";
+import NotFoundPage from "../../public/NotFoundPage";
 
 import { createBrowserRouter } from "react-router-dom";
 
